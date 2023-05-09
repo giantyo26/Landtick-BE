1fb4d75895f7867423d350e7b8ff089a1ab4e427
giantyo <giantyoard@gmail.com>
2023-05-09 07:49:39 +0700

    push

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -3,7 +3,6 @@ package mysql
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -18,15 +17,8 @@ func DatabaseInit() {
 		log.Fatal("Error Loading .env file")
 	}
 
-	var DB_HOST = os.Getenv("DB_HOST")
-	var DB_USER = os.Getenv("DB_USER")
-	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
-	var DB_NAME = os.Getenv("DB_NAME")
-	var DB_PORT = os.Getenv("DB_PORT")
+	dsn := "postgresql://postgres:5u0u45M6Lo9MGy4EA01a@containers-us-west-38.railway.app:6110/railway"
 
-	// dsn := "root:@tcp(localhost:3306)/waysgallery?charset=utf8mb4&parseTime=True&loc=Local"
-	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
