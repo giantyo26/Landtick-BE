40d9e3fe75fd8d6f853da245eb98c8b5e44d275c
giantyo26 <72968195+giantyo26@users.noreply.github.com>
2023-06-07 07:53:34 +0700

    Delete city_model.go

--- models/city_model.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type City struct {
-	ID        int     `json:"id" gorm:"primary_key:auto_increment"`
-	Name      string  `json:"name" gorm:"type: varchar(40)"`
-	StationID int     `json:"station_id"`
-	Station   Station `json:"station"`
-}
